ddc09c79fcd1590939b4a85a501bbd8251e96ce6
Swarup Ghosh <swarupg2@illinois.edu>
2025-05-06 13:01:49 +0530

    Download author pre-trained weights

---
+++ data-scripts/download-unixgen-pretrained-weights.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install gdown
+
+# Downloads [UniXGen](https://drive.google.com/file/d/1RBQEoYTBRKBh6L53QCE0OIXL0Da5knkY/view?usp=sharing)
+# pre-trained weights shared by original paper authors
+gdown https://drive.google.com/uc?id=1RBQEoYTBRKBh6L53QCE0OIXL0Da5knkY
+# unixgen.ckpt file should be downloaded

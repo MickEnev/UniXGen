235fb89cd6b7b8cc2648333a456102d3022b7902
Swarup Ghosh <swarupg2@illinois.edu>
2025-05-07 13:37:09 +0530

    Nit

--- tpu-scripts/install-unixgen-deps.sh
+++ tpu-scripts/install-unixgen-deps.sh
@@ -20,7 +20,7 @@ git+https://github.com/swghosh/taming-transformers.git@master
 EOF
 
 # makes sure we stick to the right version of torch and xla
-pip install torch~=2.6.0 'torch_xla[tpu]~=2.6.0'  \
+pip install torch~=2.6.0 'torch_xla[tpu]~=2.6.0' lightning==2.5.1 \
   -f https://storage.googleapis.com/libtpu-releases/index.html \
   -f https://storage.googleapis.com/libtpu-wheels/index.html \
   -r requirements.txt
